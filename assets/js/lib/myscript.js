6e49beac8b543f27b00e0dae0ab61d560bfa5b51
Paz Santander Tapia <psantandert@inacap.cl>
2018-07-20 08:16:03 -0400

    Medio ejercicio 7 listas las variablesfaltan funciones

--- assets/js/lib/myscript.js
+++ assets/js/lib/myscript.js
@@ -28,4 +28,30 @@ $( document ).ready( function (){
         $(".create__select").attr('src', '/assets/images/profiles/' + this.value);
     });
 
+    //Captura de los contenidos del formulario
+    $("create__form").submit (function(e){
+        var nombre = $("#name").val(); // la variable nombre tendrá el valor que tenga el input con la etiqueta name dentro del forulario. El .val lo que hace es obtener el valor de ese elemento y lo convierte en variable
+        //Variables a recuperar (que ya existen en el formulario)
+        var followers = $("#followers").val();
+        var likes = $("#likes").val();
+        var following = $("#following").val();
+        var author_foto = $("#author").val();
+        var author_name = $("#author").find(":selected").text();
+        var image_foto = $("#image").val(); 
+        var image_name = $("#image").find(":selected").text();
+
+        // Variable nueva para las nuevas cards que se irán generando
+        var new_card = $(".card").eq(0).clone();
+
+        //Y ahora las funciones que se le aplicarán y dónde se pondrán: en este caso al final (appendTo) del div con clase cards
+        new_card.appendTo(".cards");
+        // ahora le indico los elementos dentro de la nueva tarjeta, dónde encontrarlos (find) y luego qué ponerle dentro (attr)
+        new_card
+
+
+
+        e.preventDefault(); //para evitar que el formulario se envíe y, en su lugar, pueda ocupar esos datos
+    })
+
+
 })
