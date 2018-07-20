3373c54f8e93200e62609c28362fdd3f6efee801
Paz Santander Tapia <psantandert@inacap.cl>
2018-07-19 20:39:38 -0400

    ejercicio 4

--- assets/js/lib/myscript.js
+++ assets/js/lib/myscript.js
@@ -1,14 +1,21 @@
 $( document ).ready( function (){
-     //Apertura de la tarjeta
-     $(".card").click (function(){
-        $(this).toggleClass("card--open")
+    //Apertura de la tarjeta
+    $(".card").click (function(){
+        $(this).toggleClass("card--open");
     });
    
     //Like
    $(".card__like").click (function(e){
-        $(this).toggleClass("card__like--red");
+        $(".card__like").toggleClass("card__like--red");
+        e.stopPropagation();
         e.preventDefault();
+        // return false; también habría hecho stopPropagation + preventDefault
    });
 
+    //Botón seguir
+    $(".card__follow-btn").click (function(){
+        $(this).toggleClass("card__follow-btn--following");
+        return false;
+   });
 
 })
